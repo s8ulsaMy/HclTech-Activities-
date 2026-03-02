e17cc98c081a65f176abea9a547fe11a5e97e2a2
sanchiti <sanchiti.ag@gmail.com>
2026-03-02 11:19:27 +0530

    updatesss

---
+++ Module_5/chapter_2/Activity_2/simpleinterest/simpleinterest/src/test/java/com/example/simpleinterest/SimpleinterestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleinterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleinterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
